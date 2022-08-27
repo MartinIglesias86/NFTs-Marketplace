1692650785278d88f8e754662b55194381d09dca
MartinIglesias86 <martin.iglesias19@gmail.com>
2022-08-27 10:40:20 -0300

    Deploy configuration

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-ethers");
+require('dotenv').config();
+
+const projectId = process.env.INFURA_PROJECT_ID;
+const privateKey = process.env.DEPLOYER_SIGNER_PRIVATE_KEY;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
+    }
+  }
+};
